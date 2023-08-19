b7eba6f489813dbf2eb4a98e74969e2af1d28866
PULAK-0308 <jainpulak0308@gmail.com>
2023-08-19 16:20:52 +0530

    commit

---
+++ ecommerce/craft/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from . models import Product,Orders,OrderUpdate,Contact
+
+admin.site.register(Product)
+admin.site.register(Orders)
+admin.site.register(OrderUpdate)
+admin.site.register(Contact)
+
+
+
